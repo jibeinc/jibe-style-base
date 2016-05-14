dd697ba84bba71b5a93e0256009289d0cf2367c1
Michael Brown <mbrown@jibe.com>
2016-05-14 10:02:48 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Task for building blog when something changed:
+// gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
+// Or if you don't use bundle:
+gulp.task('build', shell.task(['jekyll build']));
+
+// Task for serving blog with Browsersync
+gulp.task('serve', function () {
+    browserSync.init({server: {baseDir: '_site/'}});
+    // Reloads page when some of the already built files changed:
+    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./_sass/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./_site/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./_sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['build', 'sass', 'serve', 'sass:watch']);
